cc4acc3aef3fbc9da21970428f07d066487dab2c
Greg Mason <gmason@nodoubleg.com>
2016-07-01 18:19:32 -0400

    update to some poop

--- clonemebro.sh
+++ clonemebro.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # prints crap to paste into a machine to bootstrap gmason
+# todo: turn this into a bunch of lxc exec calls
 echo "
 
-Paste the following to bootstrap gmason:
+# paste this as root:
 
-# assume gmason already created, as sudo user
+apt install zsh
+apt install vim-nox
+apt remove 
+adduser gmason
+cat /etc/sudoers.d/90-cloud* | sed -e s/ubuntu/gmason/g' > /etc/sudoers.d/80-gmason
 
-apt-get install zsh
-apt-get install vim-nox
+
+# paste this as gmason:
+
 git clone https://github.com/nodoubleg/oh-my-zsh.git .oh-my-zsh
 git clone https://github.com/nodoubleg/dotfiles.git
 bash dotfiles/linkmebro.sh
 chsh /bin/zsh
-
-
 
 "
