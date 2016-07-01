b6d061176254a60e0cf6baaa1464c14d8d056642
Greg Mason <gmason@nodoubleg.com>
2016-07-01 18:19:07 -0400

    add .zsh_completions to the party

--- linkmebro.sh
+++ linkmebro.sh
@@ -2,6 +2,8 @@
 # installs symlinks for dotfiles
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+rm ~/.zsh_completions
+ln -s ~/dotfiles/.zsh_completions ~/.zsh_completions
 rm ~/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 rm ~/.gvimrc
