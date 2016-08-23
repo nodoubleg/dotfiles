20ccd6d1a966f7f76e7a75309ee0a5ce5c3267da
Greg Mason <gmason@nodoubleg.com>
2016-08-23 19:29:06 -0400

    Switching from hacked up vim config to spf13 thing.
    This drastically reduces the config I maintain for vim. \o/

--- linkmebro.sh
+++ linkmebro.sh
@@ -4,7 +4,11 @@ rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 rm ~/.zsh_completions
 ln -s ~/dotfiles/.zsh_completions ~/.zsh_completions
-rm ~/.vimrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-rm ~/.gvimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+#rm ~/.vimrc
+#ln -s ~/dotfiles/.vimrc ~/.vimrc
+#rm ~/.gvimrc
+#ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+rm ~/.vimrc.local
+ln -s ~/dotfiles/.vimrc.local ~/.vimrc.local
+rm ~/.vimrc.before.local
+ln -s ~/dotfiles/.vimrc.before.local ~/.vimrc.before.local
