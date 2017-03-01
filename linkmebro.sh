9cc9ec779202243b98a783e96a44dfbe32992c68
Greg Mason <gmason@nodoubleg.com>
2017-02-28 22:23:25 -0700

    Add launchd plist for starting gpg-agent at login.

--- linkmebro.sh
+++ linkmebro.sh
@@ -14,3 +14,5 @@ rm ~/.vimrc.local
 ln -s ~/dotfiles/.vimrc.local ~/.vimrc.local
 rm ~/.vimrc.before.local
 ln -s ~/dotfiles/.vimrc.before.local ~/.vimrc.before.local
+rm ~/Library/LaunchAgents/org.gnupg.gpg-agent.plist
+ln -s org.gnupg.gpg-agent.plist ~/Library/LaunchAgents/org.gnupg.gpg-agent.plist
