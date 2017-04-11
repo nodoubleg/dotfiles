e449df4cfde780745dc4b1bb09c475e9a172f5f2
Greg Mason <gmason@ddn.com>
2017-04-10 22:04:34 -0400

    Switch to SpaceVim, easier customization.

---
+++ .SpaceVim.d/init.vim
@@ -0,0 +1,14 @@
+let g:spacevim_guifont = 'Inconsolata\ for\ Powerline\ 13'
+"let g:indentLine_faster = 1
+syn off
+let g:indentLine_enabled = 0
+syn on
+"set nowrap
+set norelativenumber
+set lazyredraw
+"let g:airline#extensions#whitespace#enabled = 0
+let g:spacevim_disabled_plugins=[
+    \ ['junegunn/fzf.vim'],
+    \ ['Yggdroot/indentLine'],
+    \ ['easymotion/vim-easymotion'],
+    \ ]
--- linkmebro.sh
+++ linkmebro.sh
@@ -9,11 +9,13 @@ ln -s ~/dotfiles/.zsh_completions ~/.zsh_completions
 #ln -s ~/dotfiles/.vimrc ~/.vimrc
 #rm ~/.gvimrc
 #ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-rm -rf ~/.vim
-ln -s ~/dotfiles/.vim ~/.vim
-rm ~/.vimrc.local
-ln -s ~/dotfiles/.vimrc.local ~/.vimrc.local
-rm ~/.vimrc.before.local
-ln -s ~/dotfiles/.vimrc.before.local ~/.vimrc.before.local
+#rm -rf ~/.vim
+#ln -s ~/dotfiles/.vim ~/.vim
+#rm ~/.vimrc.local
+#ln -s ~/dotfiles/.vimrc.local ~/.vimrc.local
+#rm ~/.vimrc.before.local
+#ln -s ~/dotfiles/.vimrc.before.local ~/.vimrc.before.local
 rm ~/Library/LaunchAgents/org.gnupg.gpg-agent.plist
 ln -s org.gnupg.gpg-agent.plist ~/Library/LaunchAgents/org.gnupg.gpg-agent.plist
+rm -rf ~/.SpaceVim.d
+ln -s ~/dotfiles/.SpaceVim.d ~/.SpaceVim.d
